94b486d7b253391d5f46daf9cf9abbfc037d57fb
Owen Carruth <ocarr199@gmail.com>
2021-04-21 15:47:42 -0500

    First run through

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,28 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+
+var basket = []
+
+function addItem(item){
+basket.push(item)
+return true
+}
+
+console.log(addItem("food"))
+
+function listItems(){
+  for(i = 0; i < basket.length; i++)
+  console.log(basket[i])
+}
+
+listItems()
+
+
+function empty(){
+  basket=[];
+}
+
+console.log(empty())
